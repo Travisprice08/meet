e16cb8d0b0bb045770b12fc8382b60649c007186
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-09-13 19:04:41 -0700

    4.3
    fixed syntax errors

--- src/EventList.js
+++ src/EventList.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import Event from './Event';
 
 class EventList extends Component {
     render() {
+        const { events } = this.props;
         return (
-            <ul></ul>
+            <ul className="EventList">
+                {events.map(event =>
+                    <li key={event.id}>
+                        <Event event={event} />
+                    </li>
+                )}
+            </ul>
         );
     }
 }
