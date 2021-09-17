6a8f9d5be031c07c990cc1756d3dfe5a8c66729b
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-09-16 18:57:38 -0700

    4.4
    Added access tokens
    
    stylized ui

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import CitySearch from '../CitySearch';
+import CitySearch from '../component/CitySearch';
 import { mockData } from '../mock-data';
 import { extractLocations } from '../api';
 
@@ -8,7 +8,7 @@ describe('<CitySearch /> component', () => {
     let locations, CitySearchWrapper;
     beforeAll(() => {
         locations = extractLocations(mockData);
-        CitySearchWrapper = shallow(<CitySearch locations={locations} />);
+        CitySearchWrapper = shallow(<CitySearch locations={locations} updateEvents={() => { }} />);
     });
     test('render text input', () => {
         expect(CitySearchWrapper.find('.city')).toHaveLength(1);
@@ -54,5 +54,21 @@ describe('<CitySearch /> component', () => {
         const suggestions = CitySearchWrapper.state('suggestions');
         CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
         expect(CitySearchWrapper.state("query")).toBe(suggestions[0]);
+    });
+
+    test("Selecting CitySearch input reveals teh suggestions list", () => {
+        CitySearchWrapper.find('.city').simulate('focus');
+        expect(CitySearchWrapper.state('showSuggestions')).toBe(true);
+        expect(CitySearchWrapper.find('.suggestions').prop('style')).not.toEqual({ display: 'none' });
+    });
+
+    test("selecting a suggestion should hide the suggestions list", () => {
+        CitySearchWrapper.setState({
+            query: 'Berlin',
+            showSuggestions: false
+        });
+        CitySearchWrapper.find('.suggestions li').at(0).simulate('click');
+        expect(CitySearchWrapper.state('showSuggestions')).toBe(false);
+        expect(CitySearchWrapper.find('.suggestions').prop('style')).toEqual({ display: 'none' });
     });
 });
