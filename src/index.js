0be698b89dc702a6d2197b5ae066e376cd463084
Travisprice08 <75191962+Travisprice08@users.noreply.github.com>
2021-10-03 12:51:49 -0700

    4.8
    added new images
    
    enabled serviceWorkers

--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import * as atatus from 'atatus-spa';
+// import * as atatus from 'atatus-spa';
 
-atatus.config('37019a187df6438c820c0b40a8846ef5').install();
+// atatus.config('37019a187df6438c820c0b40a8846ef5').install();
 
 ReactDOM.render(
   <React.StrictMode>
@@ -18,7 +18,7 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.unregister();
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
